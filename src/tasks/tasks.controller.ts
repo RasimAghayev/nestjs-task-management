62c02f58f31e71836ce6321f6c844022e15f874f
Rasim Aghayev <rasimaqayev@gmail.com>
2021-07-14 01:04:20 +0400

    Add Tasks Controller getTasks

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,16 +1,21 @@
-import {Body, Controller, Get, Param, Post} from '@nestjs/common';
+import {Body, Controller, Get, Param, Post, Query} from '@nestjs/common';
 import {TasksService} from "./tasks.service";
 import {Task, TaskStatus} from "./task.model";
 import {CreateTaskDto} from './dto/create-task.dto';
 import {Delete, Patch} from "@nestjs/common/decorators/http/request-mapping.decorator";
+import {GetTasksFilterDto} from "./dto/get-tasks-filter.dto";
 
 @Controller('tasks')
 export class TasksController {
     constructor(private tasksService: TasksService) {}
 
     @Get()
-    getAllTasks(): Task[] {
-        return this.tasksService.getAllTasks();
+    getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+        if (Object.keys(filterDto).length){
+            return this.tasksService.getTaskWithFilters(filterDto);
+        }else{
+            return this.tasksService.getAllTasks();
+        }
     }
 
     @Get('/:id')
