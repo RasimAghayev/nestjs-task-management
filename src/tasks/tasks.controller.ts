2bdc2420e56e9114e9df8393c0f10c3260199065
Rasim Aghayev <rasimaqayev@gmail.com>
2021-07-14 00:03:11 +0400

    Add deleteTask

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -2,6 +2,7 @@ import {Body, Controller, Get, Param, Post} from '@nestjs/common';
 import {TasksService} from "./tasks.service";
 import {Task} from "./task.model";
 import {CreateTaskDto} from './dto/create-task.dto';
+import {Delete} from "@nestjs/common/decorators/http/request-mapping.decorator";
 
 @Controller('tasks')
 export class TasksController {
@@ -22,5 +23,10 @@ export class TasksController {
         // console.log('title',title);
         // console.log('description',description);
         return this.tasksService.createTask(createTaskDto);
+    }
+
+    @Delete('/:id')
+    deleteTask(@Param('id') id:string):void {
+        this.tasksService.deleteTask(id);
     }
 }
